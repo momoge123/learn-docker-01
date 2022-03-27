a85dfaca7f0e6b0814effe023b8bca514e41aa41
wux <2290233221@qq.com>
2022-03-27 22:52:53 +0800

    idea git commit 02

---
+++ src/main/java/com/atguigu/docker/controller/OrderController.java
@@ -0,0 +1,28 @@
+package com.atguigu.docker.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+  * @auther zzyy
+  * @create 2021-10-25 17:43
+  */
+@RestController
+public class OrderController {
+    @Value("${server.port}")
+    private String port;
+
+    @RequestMapping("/order/docker")
+    public String HelloDocker(){
+        return "hello docker"+"\t"+ port + "\t"+ UUID.randomUUID().toString();
+    }
+
+    @RequestMapping(value ="/order/index",method = RequestMethod.GET)
+    public String index(){
+        return "服务端口号: "+"\t"+ port + "\t"+ UUID.randomUUID().toString();
+    }
+}
