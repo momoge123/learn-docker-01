e195f19d76f60c8b21269f87b63ff9007c5947ed
momoge123 <38372228+momoge123@users.noreply.github.com>
2022-03-28 20:44:52 +0800

    Update OrderController.java

--- src/main/java/com/atguigu/docker/controller/OrderController.java
+++ src/main/java/com/atguigu/docker/controller/OrderController.java
@@ -23,7 +23,6 @@ public class OrderController {
 
     @RequestMapping(value ="/order/index",method = RequestMethod.GET)
     public String index(){
-		System.out.println("-------------");
         return "服务端口号: "+"\t"+ port + "\t"+ UUID.randomUUID().toString();
     }
 }
